d80c4676e42ac13465e8c19697bae489aba25510
Michael Smitte <smittemichael@yahoo.com>
2022-10-13 18:20:52 -0400

    added notes for next time progressing in project

---
+++ assets/buttons.js
@@ -0,0 +1,3 @@
+// This file will contain the onclick functions for each button
+// on the webpage. The buttons will link to the logic.js
+//file. 
---
+++ assets/logic.js
@@ -0,0 +1,4 @@
+//This file will contain the logic for each button for the 
+//button.js file to reference. it needs to contain the random
+//number generator for each style of rng. also needs a button
+//to clear local storage for that specific style of rng.
